e8d1119de99d7061848ce08d371a3c8810a0e5a8
Ryan Bliss <smile@ryanbliss.me>
2022-07-14 21:08:24 -0700

    Installed @fluentui/react-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FluentProvider, teamsDarkTheme } from "@fluentui/react-components";
 import "./App.css";
 import PageWrapper from "./components/page-wrapper/PageWrapper";
 import SandpackLive from "./components/sandpack-live/SandpackLive";
@@ -9,13 +10,26 @@ function App() {
 
   return (
     <div className="App">
-      <PageWrapper loading={loading} error={error}>
-        <SandpackLive
-          template={"react"}
-          codePagesMap={codePagesMap}
-          container={container}
-        />
-      </PageWrapper>
+      <FluentProvider
+        theme={teamsDarkTheme}
+        style={{
+          minHeight: "0px",
+          position: "absolute",
+          left: "0",
+          right: "0",
+          top: "0",
+          bottom: "0",
+          overflow: "hidden",
+        }}
+      >
+        <PageWrapper loading={loading} error={error}>
+          <SandpackLive
+            template={"react"}
+            codePagesMap={codePagesMap}
+            container={container}
+          />
+        </PageWrapper>
+      </FluentProvider>
     </div>
   );
 }
