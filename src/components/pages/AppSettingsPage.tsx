74de794134c047c5061e852286d8f1ba77a793e6
Ryan Bliss <smile@ryanbliss.me>
2022-07-24 19:38:33 -0700

    Handled local browser case for AppSettingsPage

--- src/components/pages/AppSettingsPage.tsx
+++ src/components/pages/AppSettingsPage.tsx
@@ -2,6 +2,8 @@ import * as microsoftTeams from "@microsoft/teams-js";
 import { useEffect, useState } from "react";
 import { Title2, Subtitle2 } from "@fluentui/react-components";
 import { FlexColumn } from "../flex";
+import { inTeams } from "../../utils/inTeams";
+import PageWrapper from "../page-wrapper/PageWrapper";
 
 export const AppSettingsPage = () => {
   const [registered, setRegistered] = useState<boolean>(false);
@@ -10,31 +12,36 @@ export const AppSettingsPage = () => {
     if (registered) {
       return;
     }
-    microsoftTeams.pages.config.registerOnSaveHandler((saveEvent) => {
-      microsoftTeams.pages.config.setConfig({
-        suggestedDisplayName: "Sandbox",
-        contentUrl: `${window.location.origin}/side-panel?inTeams=true`,
+    if (inTeams()) {
+      microsoftTeams.pages.config.registerOnSaveHandler((saveEvent) => {
+        microsoftTeams.pages.config.setConfig({
+          suggestedDisplayName: "Sandbox",
+          contentUrl: `${window.location.origin}/side-panel?inTeams=true`,
+        });
+        saveEvent.notifySuccess();
       });
-      saveEvent.notifySuccess();
-      setRegistered(true);
-    });
 
-    microsoftTeams.pages.config.setValidityState(true);
+      microsoftTeams.pages.config.setValidityState(true);
+    } else {
+      setRegistered(true);
+    }
   }, [registered]);
 
   return (
-    <FlexColumn
-      expand="fill"
-      vAlign="center"
-      hAlign="center"
-      marginSpacer="small"
-    >
-      <Title2 block align="center">
-        {"Welcome to Live Share Sandbox!"}
-      </Title2>
-      <Subtitle2 block align="center">
-        {"Press the save button to continue."}
-      </Subtitle2>
-    </FlexColumn>
+    <PageWrapper loading={!registered} error={undefined}>
+      <FlexColumn
+        expand="fill"
+        vAlign="center"
+        hAlign="center"
+        marginSpacer="small"
+      >
+        <Title2 block align="center">
+          {"Welcome to Live Share Sandbox!"}
+        </Title2>
+        <Subtitle2 block align="center">
+          {"Press the save button to continue."}
+        </Subtitle2>
+      </FlexColumn>
+    </PageWrapper>
   );
 };
